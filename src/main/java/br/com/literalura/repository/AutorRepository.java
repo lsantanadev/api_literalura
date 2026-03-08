0ce4d0b0541ff77060bbf38255d9d0040d513af0
Leonardo Santana da Silva <lsantanasilva98@gmail.com>
2026-03-08 16:26:12 -0300

    adiciona classes para conexao com banco de dados

--- src/main/java/br/com/literalura/repository/AutorRepository.java
+++ src/main/java/br/com/literalura/repository/AutorRepository.java
@@ -1,4 +1,15 @@
 package br.com.literalura.repository;
 
-public interface AutorRepository {
+import br.com.literalura.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+    @Query("SELECT a FROM Autor a WHERE a.anoNascimento <= :ano AND (a.anoFalecimento >= :ano OR a.anoFalecimento IS NULL)")
+    List<Autor> buscarAutoresVivosNoAno(@Param("ano") Integer ano);
+
 }
--- src/main/java/br/com/literalura/repository/LivroRepository.java
+++ src/main/java/br/com/literalura/repository/LivroRepository.java
@@ -1,4 +1,15 @@
 package br.com.literalura.repository;
 
-public interface LivroRepository {
+import br.com.literalura.model.Idioma;
+import br.com.literalura.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    Optional<Livro> findByTituloIgnoreCase(String nomeLivro);
+
+    List<Livro> findByIdioma(Idioma idioma);
 }
