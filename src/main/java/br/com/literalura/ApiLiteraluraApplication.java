1106af1528436ba93405bbd066dbd6d97a86cf49
Leonardo Santana da Silva <lsantanasilva98@gmail.com>
2026-03-08 16:19:03 -0300

    feat: adiciona variaveis de repository

--- src/main/java/br/com/literalura/ApiLiteraluraApplication.java
+++ src/main/java/br/com/literalura/ApiLiteraluraApplication.java
@@ -1,6 +1,9 @@
 package br.com.literalura;
 
 import br.com.literalura.principal.Principal;
+import br.com.literalura.repository.AutorRepository;
+import br.com.literalura.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +11,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiLiteraluraApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiLiteraluraApplication.class, args);
-	}
+    @Autowired
+    private LivroRepository repositorio;
 
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
-		principal.exibeMenu();
-	}
+    @Autowired
+    private AutorRepository autorRepositorio;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLiteraluraApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal(repositorio, autorRepositorio);
+        principal.exibeMenu();
+    }
 }
