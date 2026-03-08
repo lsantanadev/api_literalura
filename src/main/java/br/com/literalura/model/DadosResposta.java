7a27b693837eee7575336925f4fe0d69b470c950
Leonardo Santana da Silva <lsantanasilva98@gmail.com>
2026-03-08 16:22:15 -0300

    feat: adiciona record para receber dados json 'results'

--- src/main/java/br/com/literalura/model/DadosResposta.java
+++ src/main/java/br/com/literalura/model/DadosResposta.java
@@ -1,4 +1,10 @@
 package br.com.literalura.model;
 
-public record DadosResposta() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosResposta(@JsonAlias("results") List<DadosLivro> resultados) {
 }
